90ef8449bc8bbb2378d98025fdac0b86a566bb6a
shinzy1998 <99274840+shinzy1998@users.noreply.github.com>
2022-02-09 11:26:54 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext = Extension("Random-ip", sources=["Random-ip.c"])
+ext = Extension("Shinzy24", sources=["Shinzy24.c"])
 
 setup(
 	ext_modules=[ext],
